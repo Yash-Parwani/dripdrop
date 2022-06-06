4b732b81ebebbca1efcaa7638c14aecf34e95125
Yash Parwani <parwaniyash5610@gmail.com>
2022-06-06 09:38:03 +0530

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { Navbar, Container,Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      Drip Drop
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            DripDrop
+          </Navbar.Brand>
+          <Nav className="justify-content-end">
+            <Nav.Link href="#home">Login</Nav.Link>
+            <Nav.Link href="#features">Register</Nav.Link>
+            
+          </Nav>
+        </Container>
+      </Navbar>
+     
     </div>
   );
 }
